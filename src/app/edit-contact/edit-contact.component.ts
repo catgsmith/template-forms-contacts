6ca7c5bdb50dfe3f944793502422b126cd71376a
Catherine Gaughan-Smith <catgsmith@users.noreply.github.com>
2024-11-13 19:24:41 +0000

    Defining a data model

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,14 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Contact } from '../contacts/contact.model';
+
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   standalone: true,
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
+  contact: Contact = {
+    id: '',
+    firstName: 'Fred',
+    lastName: '',
+    dateOfBirth: null,
+    favoritesRanking: 0,
+    phone: {
+      phoneNumber: '',
+      phoneType: '',
+    },
+    address: {
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    },
+  }
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -17,6 +39,6 @@ export class EditContactComponent implements OnInit {
   }
 
   saveContact() {
-
+    console.log('Save contact', this.contact);
   }
 }
